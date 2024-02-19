c4294a09e3cb48c3271e50cdd54c215ea14c6f26
Sorin Chiuchiu <104432589+ChiuchiuSorin@users.noreply.github.com>
2024-02-19 11:23:43 +0200

    View pages that have above 50 comments #29 (#30)
    * Created function inside InstanceUsage class to retrieve the list with pages above the max comments limit
    * Created tests
    * created a modal to display the pages that have above a given number of comments
    * adapted the wikiSize template
    * Updated CSS
    * Added translations
    * Added comments

--- application-admintools-default/src/main/java/com/xwiki/admintools/internal/wikiUsage/InstanceUsage.java
+++ application-admintools-default/src/main/java/com/xwiki/admintools/internal/wikiUsage/InstanceUsage.java
@@ -24,17 +24,23 @@ import java.util.Collection;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.inject.Provider;
 import javax.inject.Singleton;
 import javax.script.ScriptContext;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.xwiki.component.annotation.Component;
+import org.xwiki.model.reference.DocumentReferenceResolver;
+import org.xwiki.query.Query;
+import org.xwiki.query.QueryException;
+import org.xwiki.query.QueryManager;
 import org.xwiki.script.ScriptContextManager;
 import org.xwiki.template.TemplateManager;
 import org.xwiki.wiki.descriptor.WikiDescriptor;
 import org.xwiki.wiki.descriptor.WikiDescriptorManager;
 
+import com.xpn.xwiki.XWikiContext;
 import com.xwiki.admintools.WikiSizeResult;
 import com.xwiki.admintools.internal.data.identifiers.CurrentServer;
 
@@ -53,10 +59,19 @@ public class InstanceUsage
     private UsageDataProvider usageDataProvider;
 
     @Inject
+    private DocumentReferenceResolver<String> resolver;
+
+    @Inject
     private WikiDescriptorManager wikiDescriptorManager;
 
     @Inject
+    private Provider<XWikiContext> wikiContextProvider;
+
+    @Inject
     private Logger logger;
+
+    @Inject
+    private QueryManager queryManager;
 
     @Inject
     private CurrentServer currentServer;
@@ -94,6 +109,21 @@ public class InstanceUsage
                 ExceptionUtils.getRootCauseMessage(e));
             return null;
         }
+    }
+
+    /**
+     * Retrieves in descending order the documents that have more than a given number of comments.
+     *
+     * @param maxComments maximum number of comments below which the document is ignored.
+     * @return a {@link List} with the documents that have more than the given number of comments.
+     * @throws QueryException if the query to retrieve the document fails.
+     */
+    public List<String> getDocumentsOverGivenNumberOfComments(long maxComments) throws QueryException
+    {
+        return this.queryManager.createQuery(
+                "select obj.name from BaseObject obj where obj.className='XWiki.XWikiComments' "
+                    + "group by obj.name having count(*) > :maxComments order by count(*) desc", Query.HQL)
+            .setWiki(wikiDescriptorManager.getCurrentWikiId()).bindValue("maxComments", maxComments).execute();
     }
 
     private List<WikiSizeResult> getWikisSize()
